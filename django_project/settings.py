10c4111b45c6ad9f9eac11f59f5f68e6ef8f35ad
tom3108 <t.madziara@gmail.com>
2020-03-18 11:05:50 +0100

    Part 8. User Profile and Picture. Fixing Image bug

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
--- django_project/urls.py
+++ django_project/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
