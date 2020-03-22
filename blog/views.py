db1525bf63f0c7323c58d57b8175208abcdc2909
tom3108 <t.madziara@gmail.com>
2020-03-22 22:00:11 +0100

    Part 10. Create, Update and Delete Posts. CreateView

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (ListView, 
+	DetailView, 
+	CreateView
+)
 from .models import Post
 
 
@@ -17,6 +21,15 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+    	form.instance.author = self.request.user
+    	return super().form_valid(form)
+
 
 
 def about(request):
